8492d3148e024fa81b3fe7161b4bde240f9af080
caique.moraes <caique.moraes@compasso.com.br>
2022-10-08 19:10:52 -0300

    sourcemap true

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   splitting: true,
-  sourcemap: false,
+  sourcemap: true,
   clean: true,
   minify: true,
   dts: true,
