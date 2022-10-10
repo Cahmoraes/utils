1b159a20d37a30f731227e661aa3686dd7c07c20
caique.moraes <caique.moraes@compasso.com.br>
2022-10-10 15:16:02 -0300

    takeUntil enhancements

--- src/utils/takeUntil.ts
+++ src/utils/takeUntil.ts
@@ -3,4 +3,4 @@ type Func<T> = (...args: any[]) => T
 export const takeUntil =
   <T>(fn: Func<T>, until = Infinity) =>
   (...args: unknown[]): Func<T> | undefined =>
-    until-- ? (Reflect.apply(fn, null, args) as Func<T>) : undefined
+    until-- > 0 ? (Reflect.apply(fn, null, args) as Func<T>) : undefined
