4c526aad4b469d6b14d90aea48368c1652637e5e
caique.moraes <ike.moraes93@gmail.com>
2022-12-07 14:36:48 -0300

    update curry types

--- src/utils/curry.ts
+++ src/utils/curry.ts
@@ -1,12 +1,37 @@
-type Curry = <R extends (...args: any) => any>(fn: R) => Curried<ReturnType<R>>
+type Func<TS extends any[], R> = (...args: TS) => R
 
-// type Curried<R> = (...args: any) => R | Curried<R>
-type Curried<R> = (...args: any) => Curried2<R>
+interface Curry {
+  <A, R>(func: (arg: A) => R): CurriedFunction1<A, R>
+  <A1, A2, R>(func: (arg1: A1, arg2: A2) => R): CurriedFunction2<A1, A2, R>
+  <A1, A2, A3, R>(func: (arg1: A1, arg2: A2, arg3: A3) => R): CurriedFunction3<
+    A1,
+    A2,
+    A3,
+    R
+  >
+  (func: Func<any[], any>): Func<any[], any>
+}
+interface CurriedFunction1<A, R> {
+  (): CurriedFunction1<A, R>
+  (arg: A): R
+}
+interface CurriedFunction2<A1, A2, R> {
+  (): CurriedFunction2<A1, A2, R>
+  (arg1: A1): CurriedFunction1<A2, R>
+  (arg1: '_', arg2: A2): CurriedFunction1<A1, R>
+  (arg1: A1, arg2: A2): R
+}
 
-type Curried2<R> = R extends (...args: any) => any ? never : Curried<R>
+interface CurriedFunction3<A1, A2, A3, R> {
+  (): CurriedFunction3<A1, A2, A3, R>
+  (arg1: A1): CurriedFunction2<A1, A2, R>
+  (arg1: '_', arg2: A2): CurriedFunction1<A1, R>
+  (arg1: A1, arg2: A2): CurriedFunction3<A1, A2, A3, R>
+  (arg1: A1, arg2: A2, arg3: A3): R
+}
 
-export const curry: Curry = (fn) => {
-  return function curried(...args: any) {
+export const curry: Curry = (fn: Func<any[], any>) => {
+  function curried(...args: any[]) {
     if (fn.length <= args.length) {
       return Reflect.apply(fn, null, args)
     } else {
@@ -15,4 +40,6 @@ export const curry: Curry = (fn) => {
       }
     }
   }
+
+  return curried
 }
--- test/curry.test.ts
+++ test/curry.test.ts
@@ -17,25 +17,27 @@ describe('curry test suite', () => {
     expect(curried1).toBeInstanceOf(Function)
     expect(curried2).toBeInstanceOf(Function)
 
-    if (typeof curried2 === 'function') expect(curried2(2)).toBe(4)
+    if (typeof curried2 === 'function') expect(curried2('2')).toBe('22')
   })
 
   it('should return value when passed all arguments to the function', () => {
     const sum = (n1: number, n2: string) => n1 + n2
     const curried = curry(sum)
-    const value = curried(2, 2)
+    const value = curried(2, '2')
 
     expect(curried).toBeInstanceOf(Function)
-    expect(value).toBe(4)
+    expect(value).toBe('22')
   })
 
   it('should return one function by argument ignoring not necessary arguments', () => {
     const sum = (n1: number, n2: number, n3: number) => n1 + n2 + n3
     const curried = curry(sum)
-    const value = curried(2, 2, 4, 5)
+    const value = curried(2, 2, 4)
 
     expect(curried).toBeInstanceOf(Function)
     expect(value).toBe(8)
     expect(curried(2)(2)(2)).toBe(6)
+    expect(curried(2)(2, 2)).toBe(6)
+    expect(curried(2, 2, 2)).toBe(6)
   })
 })
